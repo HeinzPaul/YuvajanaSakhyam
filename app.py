a9c33ebe5b259b091f518f53f7748f8e50eef151
HeinzPaul <heinzbinjupaul@gmail.com>
2025-07-03 20:39:56 +0530

    commiittee members

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, jsonify, url_for
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+UPLOAD_FOLDER = os.path.join('static', 'avatars')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
@@ -9,6 +14,19 @@ def home():
 @app.route('/about')
 def about():
     return "This is the about page."
+
+@app.route('/upload-avatar', methods=['POST'])
+def upload_avatar():
+    file = request.files.get('avatar')
+    member = request.form.get('member', 'avatar')
+    if file:
+        ext = os.path.splitext(file.filename)[1].lower()
+        filename = secure_filename(f"{member}{ext}")
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        url = url_for('static', filename=f'avatars/{filename}')
+        return jsonify(success=True, url=url)
+    return jsonify(success=False), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
